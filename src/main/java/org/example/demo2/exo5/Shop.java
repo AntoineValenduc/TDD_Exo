4b2cfad9c46123839d0106a00136fead958f5ee4
AntoineValenduc <ant.valenduc@gmail.com>
2024-10-17 14:09:53 +0200

    Test OK

--- src/main/java/org/example/demo2/exo5/Shop.java
+++ src/main/java/org/example/demo2/exo5/Shop.java
@@ -16,7 +16,8 @@ public class Shop {
     private List<Product> products;
 
     public void update(Product product) {
-        //throw new NotImplementedException();
+        product.setQuality(product.getQuality() - 1);
+        product.setSellIn(product.getSellIn() - 1);
     }
 
 }
