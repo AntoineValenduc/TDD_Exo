f06604df578d5a48b0590c67b3b5961cb205128d
AntoineValenduc <ant.valenduc@gmail.com>
2024-10-17 15:26:37 +0200

    test OK

--- src/main/java/org/example/demo2/exo5/Shop.java
+++ src/main/java/org/example/demo2/exo5/Shop.java
@@ -22,14 +22,19 @@ public class Shop {
         int newQuality = 0;
         int newSellIn = 0;
 
-        if (oldQuality != 0 && oldSellIn != 0){
-            newQuality = oldQuality -1;
-            newSellIn = oldSellIn -1;
+        if (product.getName().equals("Brie Vieilli")) {
+            newQuality = oldQuality + 1;
         } else {
-            if (product.getSellIn() == 0){
-                newQuality = oldQuality - 2;
+            if (oldQuality != 0 && oldSellIn != 0){
+                newQuality = oldQuality -1;
+                newSellIn = oldSellIn -1;
+            } else {
+                if (product.getSellIn() == 0){
+                    newQuality = oldQuality - 2;
+                }
             }
         }
+
 
         product.setQuality(Math.max(0, Math.min(50, newQuality)));
         product.setSellIn(Math.max(0, newSellIn));
